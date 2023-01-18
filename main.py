f67a14bcc3d1b5ff85e195000e9d96fc422bf4df
machangzhe <machangzhe@qq.com>
2023-01-19 01:35:27 +0800

    test multi classification

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.tensorflow.binary_classification import binary_classification
 from src.tensorflow.feature_cross import feature_cross
+from src.tensorflow.multi_classification import multi_classification
 
 if __name__ == '__main__':
     # test_np()
@@ -7,4 +8,5 @@ if __name__ == '__main__':
     # test_lr()
     # valid_and_test_set()
     # feature_cross()
-    binary_classification()
+    # binary_classification()
+    multi_classification()
